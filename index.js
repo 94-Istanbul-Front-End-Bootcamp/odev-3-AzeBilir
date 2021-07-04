96af5418a48251e9a651238c073f174990c80d62
AzeBilir <azebiliraze@gmail.com>
2021-07-04 17:57:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+function IdCheck() {
+  if (name.value === "") {
+    errorName.style.display = "block";
+  } else {
+    errorName.style.display = "none";
+  }
+}
+function PassCheck() {
+  if (sifre.value === "") {
+    errorPassword.style.display = "block";
+  } else if (sifre.value.length > 7) {
+    errorPassword.style.display = "none";
+  } else {
+    errorPassword.style.display = "block";
+    alert("Şifreniz en az 8 karakter olmalıdır!");
+  }
+}
+
+function mailCheck() {
+  if (mail.includes("@")) {
+    errorMail.style.display = "none";
+  } else {
+    errorMail.style.display = "block";
+  }
+}
+
+function CheckBox() {
+  if (checkBox.checked) {
+    errorBox.style.display = "none";
+  } else {
+    errorBox.style.display = "block";
+  }
+}
+
+function ButtonCheck() {
+  if (sifre.value === "" || mail.value === "" || name.value === "") {
+    alert("Lütfen zorunlu alanları doldurunuz!");
+  } else {
+    return;
+  }
+}
